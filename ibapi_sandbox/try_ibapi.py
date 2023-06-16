3c74eb0fee3d970499c07cc5a4d112662c5c3fc1
Gur Shalev <shalevgur1@gmail.com>
2023-06-16 19:29:45 +0300

    Adding documentation for ibapi example
    Adding documentation for ibapi example

--- ibapi_sandbox/try_ibapi.py
+++ ibapi_sandbox/try_ibapi.py
@@ -6,6 +6,13 @@ from ibapi.contract import Contract
 import threading
 import time
 
+
+# An implementaion of the EWrapper perant class.
+# The EWrapper is a callback interface for the EClient
+# that responsible (the EClient) for interacting with the
+# IB server and call the relevant method in the EWrapper to handle
+# the recieved data. Here I emplemnt callback methods and customize
+# them to my needs
 class IBapi(EWrapper, EClient):
     def __init__(self):
         EClient.__init__(self, self)
@@ -24,7 +31,7 @@ app = IBapi()
 app.connect('127.0.0.1', 7497, 123)
 
 # Start the socket connection in a different thread
-# for not interfir it in the main block of the script
+# to not interfir it in the main block of the script
 # by other commands
 socket_thread = threading.Thread(target=run_loop, daemon=True)
 socket_thread.start()
